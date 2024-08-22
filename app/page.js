74b202c6094751d6e4539ded508f1b979f495004
Ayush Gupta <ayushgupta01195@gmail.com>
2024-08-22 19:25:49 +0530

    Add Aceternity UI and shacn/ui

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+import ExpandableCardDemo from "@/components/blocks/expandable-card-demo-grid";
 import Image from "next/image";
 
 
 export default function Home() {
   return (
       <>
-      <h1 className="text-3xl font-bold">Student&apos;s Gymkhana - National Institute of Technology, Durgapur</h1>
+      {/* <h1 className="text-3xl font-bold">Student&apos;s Gymkhana - National Institute of Technology, Durgapur</h1> */}
       </>
 
   );
