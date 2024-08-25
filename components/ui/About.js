99751667536ccf7f5db18a0f21f5db30c4b28b72
sagardas25 <sagar.xyz05@gmail.com>
2024-08-25 16:18:39 +0530

    fixed alignment of about and noticeboard

--- components/ui/About.js
+++ components/ui/About.js
@@ -5,10 +5,10 @@ import { Bounce, Fade, Hinge } from "react-awesome-reveal";
 export default function About() {
   return (
     <Fade>
-      <div className=" m-10 p-7 bg-gray-100 rounded-lg shadow-md">
+      <div className=" p-5 mt-10 bg-gray-100 border border-gray-300 rounded-lg shadow-md lg:mr-5 sm:mr-0">
         <h2 className="text-3xl font-bold text-primary mb-4">About</h2>
         <p className="text-lg text-gray-800 mb-6">
-          Students’ Gymkhana (SG) aims to provide students a role in the administrative and academic governance of the institute and supports them to develop leadership and administrative skills. We operate in a dynamic manner under the guidance of our faculty advisers.
+          Students&appos; Gymkhana (SG) aims to provide students a role in the administrative and academic governance of the institute and supports them to develop leadership and administrative skills. We operate in a dynamic manner under the guidance of our faculty advisers.
         </p>
         <a
           href="https://nitdgp.ac.in/uploads/old/academics/notices/2019/06/19/Gymkhana_Constitution.pdf" // Adjust this URL as needed
--- components/ui/Noticeboard.js
+++ components/ui/Noticeboard.js
@@ -4,7 +4,7 @@ import { Bounce, Fade } from "react-awesome-reveal";
 const NoticeBoard = () => {
   return (
     <Fade>
-    <div className="p-5 mt-10 bg-gray-100 border border-gray-300 rounded-lg shadow-md">
+    <div className="p-5 mt-10 bg-gray-100 border border-gray-300 rounded-lg shadow-md ">
       <h2 className="text-xl font-bold mb-2">Notice Board</h2>
       <p>Important announcements and notices will be displayed here.</p>
       {/* Add more content or notices here */}
