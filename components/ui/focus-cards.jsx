4fc2c7522548a91b8482f8134edbb93a8fa738df
roy-abir05 <royabirdhn@gmail.com>
2024-09-01 23:56:59 +0530

    Fixed Post Bearer Page

--- components/ui/focus-cards.jsx
+++ components/ui/focus-cards.jsx
@@ -13,21 +13,21 @@ export const Card = React.memo(({
     onMouseEnter={() => setHovered(index)}
     onMouseLeave={() => setHovered(null)}
     className={cn(
-      "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out",
+      "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden w-[80%] mx-auto h-60 md:h-96 md:w-full transition-all duration-300 ease-out",
       hovered !== null && hovered !== index && "blur-sm scale-[0.98]"
     )}>
     <Image
       src={card.src}
       alt={card.title}
       fill
-      className="object-cover absolute inset-0" />
+      className="object-cover absolute inset-0 contain-strict" />
     <div
       className={cn(
-        "absolute inset-0 bg-black/50 flex items-end py-8 px-4 transition-opacity duration-300",
+        "absolute inset-0 bg-black/50 flex items-end px-0 py-10 md:py-16 text-nowrap transition-opacity duration-300",
         hovered === index ? "opacity-100" : "opacity-0"
       )}>
       <div
-        className="text-xl md:text-2xl font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200">
+        className="text-xl md:text-2xl mx-auto font-extrabold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200">
         {card.title}
       </div>
     </div>
@@ -43,7 +43,7 @@ export function FocusCards({
 
   return (
     (<div
-      className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto md:px-8 w-full mb-5">
+      className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto md:px-8 w-full mt-6 mb-6 md:mt-0 md:mb-5">
       {cards.map((card, index) => (
         <Card
           key={card.title}
