9a04b8961793f991fd7af38bb79df12d2e7ad7dc
roy-abir05 <royabirdhn@gmail.com>
2024-10-07 16:54:50 +0530

    Made Post Bearer Page

--- app/post-bearer/page.js
+++ app/post-bearer/page.js
@@ -1,31 +1,74 @@
+"use client";
+import PostBearerCard from "@/components/PostBearerCard/PostBearerCard";
 import { FocusCards } from "@/components/ui/focus-cards";
+import { motion } from "framer-motion";
+import { LampContainer } from "@/components/ui/lamp";
 
 export default function PostBearer() {
   const cards = [
     {
-      title: "PRESIDENT",
+      name: "PRASANT SAHU",
+      post: "PRESIDENT",
       src: "/PostBearers/Presi.jpeg",
     },
     {
-      title: "VICE PRESIDENT",
+      name: "SHIVANSHU KUMAR",
+      post: "VICE PRESIDENT",
       src: "/PostBearers/VP.jpeg",
     },
     {
-      title: "GENERAL SECRETARY",
+      name: "ANKAN DATTA",
+      post: "GENERAL SECRETARY",
       src: "/PostBearers/GenSec.jpeg",
     },
     {
-      title: "AGS - SOCIAL & SPORTS",
+      name: "SUJAL KUMAR",
+      post: "AGS - SOCIAL & SPORTS",
       src: "/PostBearers/Ags_Sports.jpeg",
     },
     {
-      title: "AGS - CULTURAL",
+      name: "ARKARUP MITRA",
+      post: "AGS - CULTURAL",
       src: "/PostBearers/Ags_Culture.jpeg",
     },
     {
-      title: "GIRLS REPRESENTATIVE",
+      name: "KONDURU PRANAVI",
+      post: "GIRLS REPRESENTATIVE",
       src: "/PostBearers/GR.jpeg",
     },
   ];
-  return <FocusCards cards={cards} />;
+  return (
+    <div className="w-screen min-h-screen">
+      <div className="w-full h-[30vh] md:h-fit mt-16 md:mt-0 mx-auto flex items-center justify-center">
+        <LampContainer>
+          <motion.h1
+            initial={{ opacity: 0.5, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.3,
+              duration: 0.8,
+              ease: "easeInOut",
+            }}
+            className="bg-gradient-to-br from-slate-300 to-slate-500 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+          >
+            Post Bearers
+          </motion.h1>
+        </LampContainer>
+      </div>
+
+      <div className="w-screen h-fit flex items-center justify-center">
+        <div className="w-full h-fit mx-auto flex flex-col items-center justify-center md:grid md:grid-cols-3 md:grid-rows-2 md:gap-x-[15%] md:gap-y-[5%] md:px-10">
+          {cards.map((card, key) => (
+            <div className="w-[90%] md:w-full h-[700px]">
+              <PostBearerCard
+                src={card.src}
+                post={card.post}
+                name={card.name}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/NavBarLinks/NavBarLinks.jsx
+++ components/NavBarLinks/NavBarLinks.jsx
@@ -5,7 +5,7 @@ const NavBarLinks = (props) => {
   return (
     <div className={`min-w-fit8 h-12 px-5 py-2 bg-black flex items-center justify-center gap-11 rounded-full border-white border-[1px] shadow shadow-white ${(props.isScrolled && !props.isScrollingUp) ? 'hidden':'visible'} ${props.isScrolled?'shadow-md shadow-gray-400':''} transition-all duration-1000 ease-in-out overflow-hidden`}>
           <Link
-          href="#"
+          href="/"
           className="group text-white text-lg font-medium whitespace-nowrap hover:text-neutral-300">
             Home
             <span className="block w-0 group-hover:w-full transition-all duration-500 h-0.5 bg-neutral-400"></span>
@@ -21,9 +21,9 @@ const NavBarLinks = (props) => {
             Clubs
             <span className="block w-0 group-hover:w-full transition-all duration-500 h-0.5 bg-neutral-400"></span>
           </Link>
-          <Link href="#"
+          <Link href="/post-bearer"
           className="group text-white text-lg font-medium whitespace-nowrap hover:text-neutral-300">
-            Office Bearers
+            Post Bearers
             <span className="block w-0 group-hover:w-full transition-all duration-500 h-0.5 bg-neutral-400"></span>
           </Link>
           <button className="text-white text-lg font-medium whitespace-nowrap px-4 py-1 border border-neutral-700 rounded-full">
