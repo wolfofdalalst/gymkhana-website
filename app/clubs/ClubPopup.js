7bb550a2c41e81212a347d1831b377b4219d5fa0
sagardas25 <sagar.xyz05@gmail.com>
2024-10-27 01:12:36 +0530

    modified pop up view of clubs

--- app/clubs/ClubPopup.js
+++ app/clubs/ClubPopup.js
@@ -6,6 +6,13 @@ import { TbWorld } from "react-icons/tb";
 const ClubPopup = ({ club, onClose }) => {
   const [isVisible, setIsVisible] = useState(false);
 
+
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      handleClose();
+    }
+  };
+
   useEffect(() => {
     setIsVisible(true); 
     return () => setIsVisible(false); 
@@ -17,13 +24,14 @@ const ClubPopup = ({ club, onClose }) => {
   };
 
   return (
-    <div className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 transition-opacity duration-500 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+    <div className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 transition-opacity duration-500 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0'}`}
+    onClick={handleOverlayClick}>
       <div className={`bg-white rounded-lg p-6 w-80 md:w-96 relative transform transition-all duration-300 ease-in-out ${isVisible ? 'scale-100 opacity-100 translate-y-0' : 'scale-95 opacity-0 translate-y-4'}`}>
         <button
           onClick={handleClose}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-800 font-bold text-xl"
         >
-          &times; {/* Close button */}
+          &times; 
         </button>
         <h2 className="text-xl text-center font-bold mb-2">{club.name}</h2>
         <p className="text-gray-700 text-center mb-4">{club.description}</p>
