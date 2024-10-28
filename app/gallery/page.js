e4c39175b76ccbc48102391246799ec027831722
roy-abir05 <royabirdhn@gmail.com>
2024-10-28 18:42:58 +0530

    Modified Gallery

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,9 +1,10 @@
 "use client";
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { motion, AnimatePresence } from "framer-motion";
 
 const images = [
   "/Gallery/1.jpg",
@@ -46,7 +47,8 @@ const Gallery = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000
+    autoplaySpeed: 3000,
+    afterChange: (currentIndex) => setActiveIndex(currentIndex),
   };
 
   const handleThumbnailClick = (index) => {
@@ -61,14 +63,17 @@ const Gallery = () => {
       <h1 className="text-4xl font-bold text-white mb-8">Gallery</h1>
 
       {/* Slick Slider Section */}
-      <Slider {...settings} ref={setSliderRef} className="w-full max-w-4xl">
+      <Slider {...settings} ref={setSliderRef} className="w-full max-w-[65vw]">
         {images.map((src, index) => (
-          <div key={index} className="relative w-full h-80 md:h-96 overflow-hidden">
+          <div
+            key={index}
+            className="relative w-full h-80 md:h-[50vh] overflow-hidden"
+          >
             <Image
               src={src}
               alt={`Gallery Image ${index + 1}`}
               fill
-              style={{ objectFit: 'cover' }}
+              style={{ objectFit: "cover" }}
               className="rounded-lg transition-opacity duration-500 ease-in-out opacity-100"
             />
             <p className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-lg text-center py-2">
@@ -79,22 +84,45 @@ const Gallery = () => {
       </Slider>
 
       {/* Thumbnail Grid Section */}
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 w-full max-w-4xl px-4 md:px-0 mt-8">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 w-full max-w-[65vw] px-4 md:px-0 mt-8 z-20">
         {images.map((src, index) => (
           <div
             key={index}
-            className={`w-full h-24 md:h-32 lg:h-40 relative rounded-lg overflow-hidden shadow-md transition-transform duration-500 ease-in-out transform hover:scale-110 ${activeIndex === index ? 'border-4 border-blue-500' : ''}`}
+            className={`w-full h-24 md:h-32 lg:h-40 relative group block rounded-lg shadow-md transition-transform duration-500 ease-in-out transform hover:scale-110 ${
+              activeIndex === index ? "" : ""
+            }`}
             onClick={() => handleThumbnailClick(index)} // Update the main image on click
             role="button"
             aria-label={`View image ${index + 1}`}
           >
+            <AnimatePresence>
+              {activeIndex === index && (
+                <motion.span
+                  className="absolute inset-[-2%] h-[105%] w-[105%] bg-blue-800 block rounded-lg -z-40"
+                  layoutId="hoverBackground"
+                  initial={{ opacity: 0 }}
+                  animate={{
+                    opacity: 0.5,
+                    transition: { duration: 0.15 },
+                  }}
+                  exit={{
+                    opacity: 0,
+                    transition: { duration: 0.15, delay: 0.2 },
+                  }}
+                />
+              )}
+            </AnimatePresence>
+            <div className="rounded-lg h-full w-full p-4 overflow-hidden bg-black border border-white/[0.2] group-hover:border-blue-500 relative z-20">
+              <div className="relative z-50 w-full h-full">
             <Image
               src={src}
               alt={`Thumbnail Image ${index + 1}`}
               fill
-              style={{ objectFit: 'cover' }}
-              className={`rounded-lg transition-transform duration-500 ease-in-out transform hover:scale-110`}
+              style={{ objectFit: "cover" }}
+              className={`z-50 rounded-lg transition-transform duration-500 ease-in-out transform hover:scale-110`}
             />
+            </div>
+            </div>
           </div>
         ))}
       </div>
